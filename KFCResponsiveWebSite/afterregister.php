6b495ca8976db3cef11a663934785ecbf000307a
Dino Emso <48333019+dinoemso97@users.noreply.github.com>
2021-05-11 10:27:16 +0200

    Add files via upload

---
+++ KFCResponsiveWebSite/afterregister.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>KFC | BIH</title>
+<link rel="shortcut icon" type="image/png" href="logo's/kfc-3.png"/>
+<link rel="stylesheet" type="text/css" href="main.css"/>
+<link href="https://fonts.googleapis.com/css?family=Bad+Script|Coustard|Fugaz+One|Julius+Sans+One|Shrikhand|Ovo" rel="stylesheet"> 
+
+<meta charset="utf-8"/>
+<meta name="viewport" content="width= device-width , initial-scale= 1.0" />
+<meta name="author" content="Dino Emso"/>
+
+</head>
+
+
+<body>
+
+		<div id="computer-desktop-1">
+		
+		<div id="container-1">
+
+		<div id="main">
+		
+		
+		<div class="after">
+		<h2>Uspješno ste se registrovali, prijaviti se mozete putem ovog <a href="index.php">linka</a></h2>
+		</div>
+		
+		
+		
+		
+		
+		
+		</div>
+		
+		<footer class="footer-1 ">
+		<nav>
+		<ul>
+		<li><a href="index.php">naslovnica</a></li>
+		<li><a href="index.php?opcija=historija">historija</a></li>
+		<li><a href="index.php?opcija=onama">o nama</a></li>
+		<li><a href="index.php?opcija=register">registracija</a></li>
+		
+		<li><a href="index.php">prijavi se</a></li>
+		</ul>
+		</nav>
+		</footer>
+		<footer class="copy-1">
+		<h2 class="copyright">&copy; Sva prava zadržana KFC-2018</h2>
+		</footer>
+		
+		</div>
+		
+		
+		
+			
+		</div>
+		
+		
+
+
+
+</body>
+
+
+</html>
+		
+		
+		
+			
---
+++ KFCResponsiveWebSite/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>KFC | BIH FAN PAGE</title>
+<link rel="shortcut icon" type="image/png" href="logo's/kfc.png"/>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+<link rel="stylesheet" href="https://googleapis.com/css?family=Open+Sans|Lora
+|Catamaran|IM+Fell+French+Canon|Cabin|Shrikhand|Fugaz+One|Julius+Sans+One|Fredericka+the+Great|Neucha"/>
+
+
+<link href="https://fonts.googleapis.com/css?family=Bad+Script|Coustard|Fredericka+the+Great|Fugaz+One|Julius+Sans+One|Ovo|Shrikhand" rel="stylesheet">
+<link rel="stylesheet" href="https://googleapis.com/css?family=Vast+Shadow|Poor+Story"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta charset="utf-8"/>
+<meta name="viewport" content="width= device-width , initial-scale= 1" />
+<meta name="author" content="Dino Emso"/>
+
+</head>
+
+
+	<body id="body">
+	
+
+	<header class="session-header-1">
+	<a href="index.php">
+	<img src="logo/kfc.png" alt="sa"/></a>
+	
+	<div class="id-name">
+	
+	</div>
+	
+	<nav>
+	<ol>
+	<li><a href="index.php">naslovnica</a></li>
+	<li><a href="index.php?opcija=proizvodi">proizvodi<i style="font-size:14px" class=" fa"> &#xf0d7;</i></a>
+	<ol>
+	<li><a href="#">novi proizvodi</a></li>
+	<li><a href="#">košare</a></li>
+	<li><a href="#">big box</a></li>
+	<li><a href="#">meniji</a></li>
+	<li><a href="#">b-smart</a></li>
+	<li><a href="#">salate</a></li>
+	<li><a href="#">prilozi</a></li>
+	<li><a href="#">pića</a></li>
+	<li><a href="#">slastice</a></li>
+	<li><a href="#">za djecu</a></li>
+	</ol>
+	</li>
+	<li><a href="index.php?opcija=restorani">restorani<?php echo "  " ;  ?><i style="font-size:14px" class=" fa"> &#xf0d7;</i></a>
+	<ol>
+	<li><a href="#">Restoran Ferhadija</a></li>
+	<li><a href="#">Restoran Otoka</a></li>
+	<li><a href="#">Restoran SCC</a></li>
+	<li><a href="#">Restoran Mepas Mall</a></li>
+	
+	</ol>
+	
+	</li>
+	<li><a href="index.php?opcija=vijesti">vijesti<?php echo "  " ;  ?><i style="font-size:14px" class=" fa"> &#xf0d7;</i></a>
+	<ol>
+	<li><a href="#">Najnovije vijesti</a></li>
+	<li><a href="#">Novi obroci</a></li>
+	<li><a href="#">Oglasi</a></li>
+	<li><a href="#">Ciljevi za budućnost</a></li>
+	
+	</ol>
+	</li>
+	<li><a href="index.php?opcija=karijera">karijera</a></li>
+	<li><a href="index.php?opcija=kontakt">kontakt</a></li>
+	<li><a href="index.php?opcija=historija">historija</a></li>
+	<li><a href="index.php?opcija=onama">o nama</a></li>
+	
+	</ol>
+	</nav>
+	</header>
+	
+	
+	
+			<div class="error-1">
+			
+			<h2>Stranica koju ste tražili je nedostupna!</h2>
+			<p>Vratite se na <a href="index.php">pocetnu stranicu</a></p>
+			
+			</div>
+			
+			
+		<div class="computer-desktop-2">
+		<div class="session-container">
+		<div class="main">
+		
+		<section class="section">
+		<img src="img/kfc-food-3.jpg" alt="hot-wings" />
+		
+		</section>
+		
+		
+		<section class="section-1">
+		<h1>upoznajte svoj kfc!</h1>
+		</section>
+		
+		<div class="kfc-kofe">
+		<section class="section-2">
+		<img src="img/kfc-kofica.jpg" alt="kofica-1" width="400"/>
+		<div class="kfc-kofa">
+		<h3>uvijek pripremamo</h3>
+		<h2>neposredno prije</h2>
+		<h3>posluživanja</h3>
+		<h3> ________ </h3>
+		</div>
+		<div class="kofa">
+		<p>Svaki dan rucno pripremamo meso:paniramo,
+		pržimo i tostiramo.Radimo sve otpočetka - to je 
+		razlog savršenog okusa.
+		</p>
+		</div>
+		</section>
+		
+		<section class="section-3">
+		<img src="img/kfc-kofica1.png" alt="kofica-2"/>
+		<div>
+		
+		</div>
+		<div class="kfc-kofa-1">
+		<h3>svježinu cijenimo </h3>
+		<h2>iznad svega</h2>
+		<h3> ________ </h3>
+		</div>
+		<div class="kofa-1">
+		<p>Sigurni smo da bez svježih sastojaka nema dobrih 
+		obroka.Nama je to očito,zato proizvode za naše restorane
+		nabavljama i nekoliko puta tjedno.</p>
+		</div>
+		</section>
+		</div>
+		
+		<section id="1">
+		<div class="left">
+		<h2>Najbolje što možete dobiti od sendviča</h2>
+		<p>FILLER SENDVIČ</p>
+		</div>
+		<div class="right">
+		<img src="img/kfc-food.jpeg" alt="right-1"/>
+		</div>
+		</section>
+		
+		<section id="2">
+		<div class="left-1">
+		<h2>Hoćete obilan obrok?</h2>
+		<p>KFC BIG BOX je pravi izbor za vas!</p>
+		</div>
+		<div class="right-1">
+		<img src="img/kfc-food-1.jpeg" alt="right-2"/>
+		</div>
+		</section>
+		
+		<section id="3">
+		<div class="left-2">
+		<h2>Hoćete košaru punu radosti?</h2>
+		<p>Pune KFC Košare uvijek izmame osmijeh</p>
+		</div>
+		
+		<div class="right-2">
+		<img src="img/kfc-food-2.jpeg" />
+		</div>
+		</section>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		</div>
+		<div class="footer-session">
+		<footer class="footer-2 ">
+		
+		<ul>
+		<li><a href="#">meni</a></li>
+		<li><a href="#">restorani</a></li>
+		<li><a href="#">karijera</a></li>
+		<li><a href="#">kontakt</a></li>
+		</ul>
+		</footer>
+		<footer class="copy-2">
+		<img src="img/kfc-footer1.png"  width="40" />
+		<h2 class="copyright">Copyright KFC | FAN PAGE &copy;  2021</h2>
+		<p><small>designed by Dino Emso</small></p>
+		</footer>
+		</div>
+		</div>
+		</div>
+		
+		</body>
+		
+		
+		
+
+
+</html>
